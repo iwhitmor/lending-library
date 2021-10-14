ecdf27c7595388d804dbc14d2072fb1d8294f4ba
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-13 21:06:34 -0500

    add Library and ILibrary class and implement ILibrary in Library

---
+++ LendingLibrary/Library.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LendingLibrary
+{
+    public class Library : ILibrary
+    {
+        public int Count => throw new System.NotImplementedException();
+
+        public void Add(string title, string firstName, string lastName, int numberOfPages)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Book Borrow(string title)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Return(Book book)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- LendingLibrary/Program.cs
+++ LendingLibrary/Program.cs
@@ -9,11 +9,4 @@ namespace LendingLibrary
             Console.WriteLine("Lending Library");
         }
     }
-
-    public class Book
-    {
-        public string Title { get; set; }
-
-        public string Author { get; set; }
-    }
 }
